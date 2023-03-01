d2ec44305d2be08988947c3000c794cab31be04d
fiveFloorMan <harvieusegithub@gmail.com>
2023-03-01 19:07:29 +0800

    feat: if production mode will take .env

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import api from './routes/routesIndex'
 import { connectDB } from './db/db.connection'
 import bodyParser from 'body-parser';
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const app = express();
 const PORT = 3000;
