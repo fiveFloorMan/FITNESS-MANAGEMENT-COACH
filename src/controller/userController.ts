0f3cd99e67ed8a6d9fafd435a54ca1b3aa7eef16
fiveFloorMan <harvieusegithub@gmail.com>
2023-03-15 16:23:40 +0800

    UserController 改用class包起來

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -10,47 +10,46 @@ import { IUser, IsAdminLevel } from '../models/user'
 
 import '../config/passport'
 
-export const UserController = {
-    userLogin: ( req: Request, res: Response) => {
+export class UserController {
+    public static userLogin(req: Request, res: Response) {
         passport.authenticate('local', { session: false }, async (error: Error, user: any, info: any) => {
             if (error) {
                 return res.status(500).json({ error: error.message });
-                
             }
             if (!user) {
                 return res.status(401).json({ error: info.message });
             }
-        const token = await signToken({ sub: user.id }, process.env.JWT_SECRET as string, '30d');
-        return res.json({ token });
-        // 給予前端token之後讓前端自行處理跳轉
-        }) (req, res)
-    },
-    userRegister: async (req: Request, res: Response, next: NextFunction) => {
+            const token = await signToken({ sub: user.id }, process.env.JWT_SECRET as string, '30d');
+            return res.json({ token });
+        })(req, res);
+        }
+    
+    public static async userRegister(req: Request, res: Response, next: NextFunction) {
         const { name, email, password, gender, height, weight, isAdmin } = req.body;
         try {
             const existingUser = await UserModel.findOne({ email });
-            
+    
             if (existingUser) {
                 return res.status(409).json({ error: 'User already exists' });
             }
-            
+    
             const saltRounds = 10;
             const hashedPassword = await bcrypt.hash(password, saltRounds);
-            
+    
             const user = new UserModel({ name, email, password: hashedPassword, gender, height, weight, isAdmin });
-
+    
             if (!Object.values(IsAdminLevel).includes(user.isAdmin)) {
-                return res.status(400).json({ error: 'Level error.'})
+                return res.status(400).json({ error: 'Level error.' });
             }
-
+    
             const userTypeCheck: IUser = user;
             if (userTypeCheck) {
                 await user.save();
             }
-
-            return res.json({ message: `${user.name} is created successfully.`})
+    
+            return res.json({ message: `${user.name} is created successfully.` });
         } catch {
-            return next(Error)
+            return next(Error);
         }
     }
-};
+}
