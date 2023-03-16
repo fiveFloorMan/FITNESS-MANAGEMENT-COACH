141589942637eeb214ebf5bb4ff9f587f1ab23ec
fiveFloorMan <harvieusegithub@gmail.com>
2023-03-16 23:43:23 +0800

    chore: 刪除部必要的空格

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -39,27 +39,3 @@ export class FitnessActionDAO {
         }
     }
 }
-
-// export const findUser = (userId: string): Promise<IUser | null> => {
-//     return UserModel.findById(userId)
-//         .exec()  // 確保是查詢promise物件
-//         .then((user) => {
-//             if (user) {
-//                 return user;
-//             } else {
-//                 throw new Error('User not found.');
-//             }
-//         });
-// };
-
-// export const findFitnessAction = (trainingItemId: string): Promise<IFitnessAction | null> => {
-//     return FitnessActionModel.findById(trainingItemId)
-//         .exec()  // 確保是查詢promise物件
-//         .then((trainingItem) => {
-//             if (trainingItem) {
-//                 return trainingItem;
-//             } else {
-//                 throw new Error('Training Item not found.');
-//             }
-//         });
-// };
